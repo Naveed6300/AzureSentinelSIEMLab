1398e7e8a6363ce089c0aae4543b854d36e13f8d
Naveed <106249839+Naveed6300@users.noreply.github.com>
2025-03-03 19:03:30 -0500

    Custom_Security_Log_Exporter.ps1

--- Custom_Security_Log_Exporter.ps1
+++ Custom_Security_Log_Exporter.ps1
@@ -1,5 +1,5 @@
 ﻿# Get API key from here: https://ipgeolocation.io/
-$API_KEY      = "d4600b4efdef42b39828f5155041a457"
+$API_KEY      = "REDACTED"
 $LOGFILE_NAME = "failed_rdp.log"
 $LOGFILE_PATH = "C:\ProgramData\$($LOGFILE_NAME)"
 
